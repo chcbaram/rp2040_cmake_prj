445f9330ab606dfba0bc8c691c32d09b7f31c550
Hancheol Cho <chcbaram@gmail.com>
2022-03-09 11:17:42 +0900

    KB2040 보드 추가

---
+++ kb2040_led/src/ap/ap.cpp
@@ -0,0 +1,50 @@
+#include "ap.h"
+#include "neopixel.hpp"
+
+
+NeoPixel pixels(1, 17, NEO_GRB + NEO_KHZ400);
+
+
+void apInit(void)
+{
+  cliOpen(_DEF_UART1, 115200);
+  
+  pixels.begin();
+}
+
+void apMain(void)
+{
+  uint32_t pre_time;
+  uint8_t led_i = 0;
+
+  while(1)
+  {
+    if (millis()-pre_time >= 500)
+    {
+      pre_time = millis();
+      ledToggle(_DEF_LED1);
+
+
+      switch(led_i)
+      {
+        case 0:
+          pixels.setPixelColor(0, pixels.Color(255, 0, 0));
+          break;
+        case 2:
+          pixels.setPixelColor(0, pixels.Color(0, 255, 0));
+          break;
+        case 4:
+          pixels.setPixelColor(0, pixels.Color(0, 0, 255));
+          break;
+        default:
+          pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+          break;   
+      }
+      pixels.show();
+      led_i = (led_i + 1) % 6;
+    }
+
+    cliMain();
+  }
+}
+
---
+++ kb2040_led/src/common/hw/include/neopixel.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Adafruit_NeoPixel.hpp"
+
+
+typedef Adafruit_NeoPixel NeoPixel;
